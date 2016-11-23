4f52da265ad368b9aed43ef5d372fd43db3d2284
Vedmaka <god.vedmaka@gmail.com>
2016-11-23 19:20:26 +0300

    - special images handling - responsiveness - width & height options exposed as tag parameters

--- JssorGallery.class.php
+++ JssorGallery.class.php
@@ -14,24 +14,39 @@ class JssorGallery {
     	$slidesHtml = '';
 
 	    $images = explode("\n", trim($input));
+
+	    $i = 0;
 	    foreach ( $images as $image ) {
 		    $file = wfLocalFile( Title::newFromText( $image, NS_FILE ) );
 		    if ( $file && $file->exists() ) {
 			    $file->getFullUrl();
 		        $slidesHtml .=
-		        '<div data-p="144.50">
+		        '<div data-p="144.50" style="'.(($i > 0) ? "display: none;" : '').'">
                 	<img data-u="image" src="'.$file->getFullUrl().'" />
                 	<img data-u="thumb" src="'.$file->createThumb(72).'" />
             	</div>';
-
+		        $i++;
 		    }
 	    }
         
         $parser->getOutput()->addModules('ext.jssorgallery.foo');
-        
+
+	    $sliderHeight = 365;
+	    if( array_key_exists('height', $args) ) {
+	    	$sliderHeight = $args['height'];
+	    }
+	    $sliderHeightContainer = $sliderHeight + 100;
+	    $sliderWidth = 800;
+	    if( array_key_exists('width', $args) ) {
+		    $sliderWidth = $args['width'];
+	    }
+
         $template = file_get_contents(dirname(__FILE__).'/templates/default.mustache');
         $template = str_replace('{{ img_path }}', $wgScriptPath.'/extensions/JssorGallery/modules/', $template);
         $template = str_replace('{{ slides }}', $slidesHtml, $template);
+        $template = str_replace('{{ height }}', $sliderHeight, $template);
+        $template = str_replace('{{ height_container }}', $sliderHeightContainer, $template);
+        $template = str_replace('{{ width }}', $sliderWidth, $template);
 
         $html .= $parser->insertStripItem($template);
         
